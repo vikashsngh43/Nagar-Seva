2cb266ebbeee5900f69f9b5ee62a1b586bc5409e
vikashsngh43 <93762093+vikashsngh43@users.noreply.github.com>
2022-04-09 11:30:13 +0530

    forHotspotAdded

--- app/src/main/java/com/example/googleapiimplimentation/For_Hotspot.java
+++ app/src/main/java/com/example/googleapiimplimentation/For_Hotspot.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -39,18 +40,92 @@ public class For_Hotspot extends AppCompatActivity {
         show3 = (Button)findViewById(R.id.showhotspot3);
 
         ArrayList<String> gops = new ArrayList<String>();
-        ArrayList<String> store = new ArrayList<String>();
+        String[] store = new String[3];
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("user_complaint");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<String> gops = new ArrayList<String>();
+                String[] store = new String[3];
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     String ma = snapshot.child("pincode").getValue().toString();
-                    //System.out.println("pincodes are "+ ma);
+                    System.out.println("pincodes are "+ ma);
                     gops.add(ma);
                 }
+                HashMap<String, Integer> mpp
+                        = new HashMap<String, Integer>();
+                for (String i : gops) {
+                    if (mpp.containsKey(i)) {
+                        mpp.put(i, mpp.get(i) + 1);
+                    } else {
+                        mpp.put(i, 1);
+                    }
+                }
+                for (Map.Entry entry : mpp.entrySet()) {
+                    Log.e("mpp: ",  entry.getKey()+ "=" + entry.getValue());
+                    //System.out.println(entry.getKey() + " " + entry.getValue());
+                }
+                ArrayList<Integer> ans = new ArrayList<Integer>();
+                for (Map.Entry entry : mpp.entrySet()) {
+                    ans.add((Integer) entry.getValue());
+                }
+                Collections.sort(ans, Collections.reverseOrder());
+                int c=0;
+                int a=0;
+                for (Integer i : ans) {
+                    //System.out.println(i);
+                    if(a==i)
+                        continue;
+                    for (Map.Entry<String, Integer> entry : mpp.entrySet()) {
+                        if (entry.getValue() == i) {
+                            //System.out.println("pincode "+entry.getKey());
+                            Log.e("final:",entry.getKey());
 
+                            store[c] = entry.getKey();
+                            c++;
+                            if(c==3)
+                                break;
+                        }
+                        a=i;
+                    }
+                    if(c==3)
+                        break;
+                }
+                pin1.setText(store[0]);
+                pin2.setText(store[1]);
+                pin3.setText(store[2]);
+               /* for(int i=0; i<gops.size(); i++)
+                {
+                    Log.e("gops:",gops.get(i));
+                }*/
+                show1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(store[0]));
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+                        mapIntent.setPackage("com.google.android.apps.maps");
+                        startActivity(mapIntent);
+                    }
+                });
+                show2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(store[1]));
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+                        mapIntent.setPackage("com.google.android.apps.maps");
+                        startActivity(mapIntent);
+                    }
+                });
+                show3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(store[2]));
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+                        mapIntent.setPackage("com.google.android.apps.maps");
+                        startActivity(mapIntent);
+                    }
+                });
             }
 
             @Override
@@ -59,75 +134,14 @@ public class For_Hotspot extends AppCompatActivity {
             }
         });
 
-        HashMap<String, Integer> mpp
-                = new HashMap<String, Integer>();
-        for (String i : gops) {
-            if (mpp.containsKey(i)) {
-                mpp.put(i, mpp.get(i) + 1);
-            } else {
-                mpp.put(i, 1);
-            }
-        }
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        for (Map.Entry entry : mpp.entrySet()) {
-            ans.add((Integer) entry.getValue());
-        }
-        Collections.sort(ans, Collections.reverseOrder());
-        int c=0;
-        int a=0;
-        for (Integer i : ans) {
-            //System.out.println(i);
-            if(a==i)
-                continue;
-            for (Map.Entry<String, Integer> entry : mpp.entrySet()) {
-                if (entry.getValue() == i) {
-                    //System.out.println("pincode "+entry.getKey());
-                    store.add(entry.getKey());
-                    c++;
-                    if(c==3)
-                        break;
-                }
-                a=i;
-            }
-            if(c==3)
-                break;
-        }
 
-        //System.out.println(store.get(0));
-        //System.out.println(store.get(1));
-        //System.out.println(store.get(2));
 
-        pin1.setText(store.get(0));
-        pin2.setText(store.get(1));
-        pin3.setText(store.get(2));
+       // Log.d("status-text1", "Status: "+store[0]);
+       // System.out.println("pincode "+store[0]);
+        //System.out.println("pincode "+store[1]);
+        //System.out.println("pincode "+store[2]);
 
-        show1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(store.get(0)));
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
-            }
-        });
-        show2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(store.get(1)));
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
-            }
-        });
-        show3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode(store.get(2)));
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
-            }
-        });
+
 
     }
 }
